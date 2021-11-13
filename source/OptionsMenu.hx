dc227ce18a202680a60f89085168c7c29ae44c2f
RecoloredPoyo4 <82355015+RecoloredPoyo4@users.noreply.github.com>
2021-11-13 00:59:50 -0500

    update OptionsMenu.hx

--- source/OptionsMenu.hx
+++ source/OptionsMenu.hx
@@ -29,6 +29,7 @@ class OptionsMenu extends MusicBeatState
 			new DFJKOption(controls),
 			new DownscrollOption("Change the layout of the strumline."),
 			new MiddlescrollOption("Enable Middlescroll with down or up scroll."),
+			new NoDialogueOption("Great if you have a bad PC."),
 			new GhostTapOption("Ghost Tapping is when you tap a direction and it doesn't give you a miss."),
 			new Judgement("Customize your Hit Timings (LEFT or RIGHT)"),
 			#if desktop
